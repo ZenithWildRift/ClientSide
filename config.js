8846dc168a600ae501e62b06cddf4b4dbd047be8
nakullondhe <65081281+nakullondhe@users.noreply.github.com>
2021-04-20 15:10:48 +0530

    s

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// export const URL = 'https://zenithwildrift.herokuapp.com';
-export const URL = 'http://localhost:5000';
+export const URL = 'https://zenithwildrift.herokuapp.com';
+// export const URL = 'http://localhost:5000';
 // export const URL = 'https://api.zenithleagues.com';
 
 export default URL;
